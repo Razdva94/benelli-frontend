5e4d075c38a68fcf8b892f2cca7de65c7cf6ef2d
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-10-16 13:11:42 +0300

    indixation yandex add

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
       <div className={footerStyles.footer__container}>
-        <div className={footerStyles.footer__sectorContainer}>
+        <div className={footerStyles.footer__sectorContainer} style={{flexDirection:'row'}}>
           <div className={footerStyles.footer__imageContainer}>
             <Image
               src={logo}
@@ -20,31 +20,75 @@ const Footer = () => {
               width={200}
             />
           </div>
+          <Image
+            src={telegram}
+            alt='телега'
+            className={footerStyles.footer__icon}
+          />
           <div className={footerStyles.footer__iconsContainer}>
-            <Image
+            {/* <Image
               src={vk}
               alt='вконтакте'
               className={footerStyles.footer__icon}
-            />
-            <Image
-              src={telegram}
-              alt='телега'
-              className={footerStyles.footer__icon}
-            />
-            <Image
+            /> */}
+
+            {/* <Image
               src={instagram}
               alt='инста'
               className={footerStyles.footer__icon}
-            />
+            /> */}
           </div>
         </div>
         <div className={footerStyles.footer__sectorContainer}>
-          <h5 className={footerStyles.footer__linkHeader} id='contacts'>Контакты</h5>
-          <p style={{ textTransform: 'uppercase', color: 'var(--main-light)', fontSize: '.8rem'}}>Мотоцентр Арсенал</p>
-          <p style={{color: 'var(--main-light)', margin: '0', fontSize: '.8rem'}}>Официальный дилер BENELLI</p>
-          <p style={{color: 'var(--main-light)', margin: '0', fontSize: '.8rem'}}>в Санкт-Петербурге</p>
-          <p style={{color: 'var(--main-light)', margin: '0', fontSize: '.8rem', marginTop:'1rem'}}>Пискаревский проспект 144АК</p>
-          <p style={{color: 'var(--main-light)', margin: '0', fontSize: '.8rem'}}>+7(812) 456-1903</p>
+          <h5 className={footerStyles.footer__linkHeader} id='contacts'>
+            Контакты
+          </h5>
+          <p
+            style={{
+              textTransform: 'uppercase',
+              color: 'var(--main-light)',
+              fontSize: '.8rem',
+            }}
+          >
+            Мотоцентр Арсенал
+          </p>
+          <p
+            style={{
+              color: 'var(--main-light)',
+              margin: '0',
+              fontSize: '.8rem',
+            }}
+          >
+            Официальный дилер BENELLI
+          </p>
+          <p
+            style={{
+              color: 'var(--main-light)',
+              margin: '0',
+              fontSize: '.8rem',
+            }}
+          >
+            в Санкт-Петербурге
+          </p>
+          <p
+            style={{
+              color: 'var(--main-light)',
+              margin: '0',
+              fontSize: '.8rem',
+              marginTop: '1rem',
+            }}
+          >
+            Пискаревский проспект 144АК
+          </p>
+          <p
+            style={{
+              color: 'var(--main-light)',
+              margin: '0',
+              fontSize: '.8rem',
+            }}
+          >
+            +7(812) 456-1903
+          </p>
         </div>
       </div>
     </footer>
