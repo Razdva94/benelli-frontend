a6689ddaff6b93031b49bd34171e4efd2e2e4bac
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-19 08:29:03 +0300

    change image

--- app/[motorcycles]/page.tsx
+++ app/[motorcycles]/page.tsx
@@ -1,44 +1,14 @@
-// import React from 'react';
-// import MotoCard from '@/components/MotoCard/MotoCard';
-
-// const page = ({ params }: { params: { motorcycles: string } }) => {
-//   console.log(params)
-//   return (
-//     <MotoCard params={params}
-//     />
-//   );
-// };
-
-// export default page;
-
-
 import React from 'react';
 import MotoCard from '@/components/MotoCard/MotoCard';
 
-const Page = ({ motorcycleData }: { motorcycleData: any }) => {
-  // Используйте данные в компоненте страницы
-  return <MotoCard data={motorcycleData} />;
+const page = ({ params }: { params: { motorcycles: string } }) => {
+  console.log(params)
+  return (
+    <MotoCard params={params}
+    />
+  );
 };
 
-export async function getStaticPaths() {
-  // Получите динамические пути для предварительной генерации страниц
-  const paths = /* логика получения путей */;
-  
-  return {
-    paths,
-    fallback: true, // Установите true, если есть возможность появления новых страниц после сборки
-  };
-}
+export default page;
 
-export async function getStaticProps({ params }: { params: { motorcycles: string } }) {
-  // Получите данные для конкретной страницы на основе параметра
-  const motorcycleData = /* логика получения данных */;
-  
-  return {
-    props: {
-      motorcycleData,
-    },
-  };
-}
 
-export default Page;
