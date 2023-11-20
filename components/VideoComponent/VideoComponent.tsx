b9664a5ea71d2b72b2ac268725f019f745b8b037
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-20 13:30:31 +0300

    change

--- components/VideoComponent/VideoComponent.tsx
+++ components/VideoComponent/VideoComponent.tsx
@@ -1,33 +1,14 @@
-// import React from 'react';
-// import videoStyles from './videoComponent.module.scss';
-
-
-// const VideoComponent = () => {
-//   return (
-//     <section>
-//       <video width='1080' height='360'controls autoPlay muted className={videoStyles.videoComponent}>
-//         <source src='/video/Benelli.mp4' type='video/mp4' />
-//         Ваш браузер не поддерживает тег video.
-//       </video>
-//     </section>
-//   );
-// };
-
-// export default VideoComponent;
-'use client'
-import React, {Suspense} from 'react';
+import React from 'react';
 import videoStyles from './videoComponent.module.scss';
-import Preloader from '../Preloader/Preloader';
 
-const LazyVideo = React.lazy(() => import('./LazyVideo.tsx'));
 
 const VideoComponent = () => {
-
   return (
-    <section className={videoStyles.videoSection}>
-      <Suspense fallback={<Preloader />}>
-        <LazyVideo />
-      </Suspense>
+    <section>
+      <video width='1080' height='360'controls autoPlay muted className={videoStyles.videoComponent}>
+        <source src='/video/Benelli.mp4' type='video/mp4' />
+        Ваш браузер не поддерживает тег video.
+      </video>
     </section>
   );
 };
