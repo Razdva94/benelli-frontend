aa75d5379c9610b215f23d8f86a3b777917a516b
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-20 12:35:34 +0300

    preload change

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,18 @@ import './globals.scss';
 import MotoList from '@/components/MotoList/MotoList';
 import Contacts from '@/components/Contacts/Contacts';
 import React, { Suspense } from 'react';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import HeaderMobile from '@/components/HeaderMobile/HeaderMobile.tsx';
 import api from '@/utils/api.ts';
+import VideoComponent from '@/components/VideoComponent/VideoComponent.tsx';
 
-const VideoComponent = dynamic(
-  () => import('../components/VideoComponent/VideoComponent.tsx') as any,
-  {
-    loading: () => <div>Loading...</div>,
-    ssr: false,
-  },
-);
+// const VideoComponent = dynamic(
+//   () => import('../components/VideoComponent/VideoComponent.tsx') as any,
+//   {
+//     loading: () => <Preloader />,
+//     ssr: false,
+//   },
+// );
 
 async function getMotorcycles() {
   const motorcycles = await api.getMotorcycles()
