9c130bb00593e6e4ab572fe15a63b2332c4a2bd5
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-22 09:37:09 +0300

    not found fix

--- app/[motorcycles]/page.tsx
+++ app/[motorcycles]/page.tsx
@@ -3,41 +3,23 @@ import MotoCard from '@/components/MotoCard/MotoCard';
 import api from '@/utils/api';
 import { notFound } from 'next/navigation';
 
-// async function getMotorcycle(params: any) {
-//   const motorcycles = await api.getMotorcycle(params);
-//   if (!motorcycles) return undefined
-//   return motorcycles;
-// }
-
 async function getMotorcycle(params: any) {
   try {
-    // Вызываем метод getMotorcycle из модуля api с переданными параметрами
     const motorcycles = await api.getMotorcycle(params);
-
     if (!motorcycles) {
-      // Если данные не были получены (например, сервер вернул пустой ответ), возвращаем undefined
-      return undefined;
+      notFound();
     }
-
     return motorcycles;
   } catch (error) {
-    // Обрабатываем ошибку
     console.error('Ошибка при запросе мотоцикла:', error);
-    // Можно вернуть undefined или бросить исключение, в зависимости от вашего подхода к обработке ошибок
-    return undefined;
+    notFound();
   }
 }
 
 const page = async ({ params }: { params: { motorcycles: string } }) => {
   const motorcycleNameWithSpaces = params.motorcycles.replace(/_/g, ' ');
   const motorcycle = await getMotorcycle({ motorcycleNameWithSpaces });
-  if (motorcycle) {
-    console.log('1', motorcycle)
-    return <MotoCard motorcycle={motorcycle} />;
-  } else {
-    console.log('2','error')
-    notFound();
-  }
+  return <MotoCard motorcycle={motorcycle} />;
 };
 
 export default page;
