7ea714f1125884d187b8499854a570e1f5a1a65d
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-22 07:24:24 +0300

    metrica install

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { SessionProviders } from '@/components/SessionProviders/SessionProviders
 import Header from '@/components/Header/Header';
 import mainStyles from './main.module.scss';
 import Footer from '@/components/Footer/Footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Мотоциклы Benelli Купить Отзывы и Цена Производитель и Модели Stels',
@@ -20,30 +21,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
+      <head>
+        <meta name='yandex-verification' content='fe483d98bcc96f41' />
+        <link rel='icon' href='./images/favicon.png' type='image/png' />
+      </head>
       <SessionProviders>
-        <head>
-          <meta name='yandex-verification' content='fe483d98bcc96f41' />
-          <link rel="icon" href='./images/favicon.png' type="image/png"/>
-        </head>
         <body>
           <Header />
           <main className={mainStyles.main}>{children}</main>
           <Footer />
+          <Script strategy='beforeInteractive'>
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(95643453, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  ecommerce:"dataLayer"
+              });
+            `}
+          </Script>
         </body>
       </SessionProviders>
     </html>
   );
 }
-
-
-// const page = async ({ params }: { params: { motorcycles: string } }) => {
-//   const motorcycleNameWithSpaces = params.motorcycles.replace(/_/g, ' ');
-//   const motorcycle = await getMotorcycle({ motorcycleNameWithSpaces });
-//   if (motorcycle) {
-//     return <MotoCard motorcycle={motorcycle} />;
-//   } else {
-//     notFound();
-//   }
-// };
-
-// export default page;
