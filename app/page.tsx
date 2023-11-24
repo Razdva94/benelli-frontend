328933686c2a2a61ee58cbe020e1e3a90f3c7fee
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-24 07:06:13 +0300

    change meta/title

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import './globals.scss';
 import MotoList from '@/components/MotoList/MotoList';
 import Contacts from '@/components/Contacts/Contacts';
@@ -5,6 +6,13 @@ import React from 'react';
 import HeaderMobile from '@/components/HeaderMobile/HeaderMobile.tsx';
 import api from '@/utils/api.ts';
 import VideoComponent from '@/components/VideoComponent/VideoComponent.tsx';
+
+export const metadata: Metadata = {
+  title: 'Мотоциклы Benelli Купить Отзывы и Цена Производитель и Модели Stels',
+  description:
+    'Benelli Leoncino 500 и 800 теперь доступны в Санкт-Петербурге. Посетите наш магазин и купите этот стильный мотоцикл. Лучшие цены и условия',
+};
+
 
 async function getMotorcycles() {
   const motorcycles = await api.getMotorcycles();
